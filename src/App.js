96e3553d7bb260063d5208b12cbbad3b52d44fc5
Danteie <aleksandar.djuratovic93@gmail.com>
2023-02-01 08:23:11 +0100

    adding personal info component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Info from './Componet/Info/Info';
 
 function App() {
   return (
     <div className="App">
       <div className='countainer'>
         <div className='sidebar'>left</div>
-        <div className='content'>right</div>
+        <div className='content'>
+          <article>
+            <div>
+              <Info />
+            </div> 
+            <div className='navigation'>
+              <button>Go Back</button>
+              <button>Next Step</button>
+            </div>
+          </article>
+        </div>
       </div>
       
     </div>
---
+++ src/Componet/Info/Info.js
@@ -0,0 +1,17 @@
+
+
+export default function Info() {
+  return (
+    <div>
+        <h1>Personal Info</h1>
+        <p>Please provide your name, email address, and phone number.</p>
+        <p>Name</p>
+        <input></input>
+        <p>Email Address</p>
+        <input></input>
+        <p>Phone Number</p>
+        <input></input>
+    </div>
+  );
+}
+
