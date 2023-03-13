60513381017fcf59cf3a836f0990da644990194b
Danteie <aleksandar.djuratovic93@gmail.com>
2023-03-13 17:05:14 +0100

    creating plan page and style

--- src/Componet/Plan/Plan.js
+++ src/Componet/Plan/Plan.js
@@ -1,4 +1,4 @@
-
+import "./Plan.css"
 
 export default function Plan() {
   return (
@@ -6,7 +6,16 @@ export default function Plan() {
         <h1 className="title">Plan</h1>
         <p className="subtext">Please provide your name, email address, and phone number.</p>
         <div className="info">
-
+            <div className="plan">
+              <div>Arcade</div>
+              <div>Advanced</div>
+              <div>Pro</div>
+            </div>
+            <div className="plantime">
+              <div>Montly</div>
+              <div><input type="checkbox"/></div>
+              <div>Yearly</div>
+            </div>
         </div>
     </div>
   );
