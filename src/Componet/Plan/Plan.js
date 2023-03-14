6670a04aee3fd765220172451f61b00a0386d6a0
Danteie <aleksandar.djuratovic93@gmail.com>
2023-03-14 18:03:39 +0100

    styling plan cards

--- src/Componet/Plan/Plan.js
+++ src/Componet/Plan/Plan.js
@@ -1,4 +1,7 @@
 import "./Plan.css"
+import arcadeImage from '../../Assets/images/icon-arcade.svg'
+import advancedImage from '../../Assets/images/icon-advanced.svg'
+import proImage from '../../Assets/images/icon-pro.svg'
 
 export default function Plan() {
   return (
@@ -7,9 +10,31 @@ export default function Plan() {
         <p className="subtext">Please provide your name, email address, and phone number.</p>
         <div className="info">
             <div className="plan">
-              <div>Arcade</div>
-              <div>Advanced</div>
-              <div>Pro</div>
+
+              <div className="plan-container">
+                <img src={arcadeImage} alt='ardace'/>
+                <div>
+                  <h3>Arcade</h3>
+                  <p>$9/mo</p>
+                </div>
+              </div>
+
+              <div className="plan-container">
+                <img src={advancedImage} alt='advanced'/>
+                <div>
+                  <h3>Advanced</h3>
+                  <p>$9/mo</p>
+                </div>
+              </div>
+
+              <div className="plan-container">
+                <img src={proImage} alt='pro'/>
+                <div>
+                  <h3>Pro</h3>
+                  <p>$9/mo</p>
+                </div>
+              </div>
+
             </div>
             <div className="plantime">
               <div>Montly</div>
