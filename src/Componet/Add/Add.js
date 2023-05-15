18f8a80a0f59454cbcfd7efad39a045f4d675bea
Danteie <aleksandar.djuratovic93@gmail.com>
2023-05-15 15:14:22 +0200

    pick add page

--- src/Componet/Add/Add.js
+++ src/Componet/Add/Add.js
@@ -1,12 +1,35 @@
-
+import './Add.css'
 
 export default function ADD() {
   return (
     <div>
         <h1 className="title">Pick add-ons</h1>
         <p className="subtext">Add-ons help enhance your gaming experience</p>
-        <div className="info">
-
+        <div className="info add">
+            <div className="adon-card">
+                <input type="radio"/>
+                <div>
+                  <h2>Online</h2>
+                  <p>Access</p>
+                </div>
+                <h2>1</h2>
+            </div>
+            <div className="adon-card">
+                <input type="radio"/>
+                <div>
+                  <h2>Online</h2>
+                  <p>Access</p>
+                </div>
+                <h2>+$1/mo</h2>
+            </div>
+            <div className="adon-card">
+                <input type="radio"/>
+                <div>
+                  <h2>Online</h2>
+                  <p>Access</p>
+                </div>
+                <h2>+$1/mo</h2>
+            </div>
         </div>
     </div>
   );
